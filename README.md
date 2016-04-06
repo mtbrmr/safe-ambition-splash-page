e32e865ba6fa0b252f5102815babf8e7a32958c1
deepfriedveggie <mtbrmr@gmail.com>
2016-04-06 22:03:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A temporary splash page.
+
+Did it need to be this complicated? No.
+
+Did I want it to be this complicated? Of course I did.
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+var prefix  = require('gulp-autoprefixer');
+var pug    = require('gulp-pug');
+
+gulp.task('default', function(){
+  console.log('running');
+});
+
+gulp.task('pug', function build() {
+  return gulp.src('*.pug')
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest('_site'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefix(['last 2 versions']))
+    .pipe(gulp.dest('./_site'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('*.pug', ['pug']);
+  gulp.watch('sass/*.sass', ['sass']);
+});
+
+gulp.task('default', ['pug', 'sass', 'watch']);
